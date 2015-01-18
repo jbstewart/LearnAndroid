027735108b92cafa474d463a6fa998e336436950
JBStewart@POBox.com <JBStewart@POBox.com>
2015-01-17 21:58:39 -0700

    Exercise 3 done with Android Studio

--- AndroidStudio/GeoQuiz/app/src/main/java/geoquiz/bignerdranch/android/playfulplatypus/com/geoquiz/QuizActivity.java
+++ AndroidStudio/GeoQuiz/app/src/main/java/geoquiz/bignerdranch/android/playfulplatypus/com/geoquiz/QuizActivity.java
@@ -2,6 +2,7 @@ package geoquiz.bignerdranch.android.playfulplatypus.com.geoquiz;
 
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -17,6 +18,9 @@ public class QuizActivity extends ActionBarActivity {
     private Button mFalseButton;
     private ImageButton mNextButton;
     private TextView mQuestionTextView;
+    private int mCurrentQuestion = 0;
+    private static final String TAG = "QuizActivity";
+    private static final String KEY_INDEX = "index";
 
     private TrueFalse[] mQuestionBank = new TrueFalse[] {
             new TrueFalse(R.string.question1, true),
@@ -25,11 +29,11 @@ public class QuizActivity extends ActionBarActivity {
             new TrueFalse(R.string.question4, true),
             new TrueFalse(R.string.question5, true),
     };
-    private int mCurrentQuestion = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Log.d(TAG, "onCreate(Bundle) called");
         setContentView(R.layout.activity_quiz);
 
         mTrueButton = (Button)findViewById(R.id.true_button);
@@ -54,14 +58,46 @@ public class QuizActivity extends ActionBarActivity {
             }
         });
 
+        if (savedInstanceState != null)
+            mCurrentQuestion = savedInstanceState.getInt(KEY_INDEX, 0);
         mQuestionTextView = (TextView)findViewById(R.id.question_textview);
-//        mQuestionTextView.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View v) {
-//                nextQuestion();
-//            }
-//        });
         displayQuestion();
+    }
+
+    @Override
+    protected void onSaveInstanceState  (Bundle outState) {
+        super.onSaveInstanceState(outState);
+        outState.putInt(KEY_INDEX, mCurrentQuestion);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.d(TAG, "onPause() called");
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.d(TAG, "onStart() called");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.d(TAG, "onStop() called");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.d(TAG, "onDestroy() called");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.d(TAG, "onResume() called");
     }
 
     private void nextQuestion() {
